b120094e8bf0413cd7c020930c9f78e04b4ede3e
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-20 00:53:34 -0700

    Create InsertionSort.java

---
+++ SortingAlgorithm/InsertionSort.java
@@ -0,0 +1,25 @@
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        insertionSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+
+// 5 6 11 12 13
