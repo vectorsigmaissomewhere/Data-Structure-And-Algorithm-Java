adb04b432b204b8f9201741ad5b6c86053c4911b
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-20 06:21:18 -0700

    Create LongestCommonSubsequence(LCS).java

---
+++ DynamicProgramming/LongestCommonSubsequence(LCS).java
@@ -0,0 +1,31 @@
+/*
+Given two sequences, find the length of the longest subsequence present in both of them. 
+A subsequence is a sequence that appears in the same relative order but not necessarily contiguous.
+*/
+public class LCS {
+    public static int lcs(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+        System.out.println("Length of LCS is: " + lcs(s1, s2));
+    }
+}
+
+// Length of LCS is: 4
