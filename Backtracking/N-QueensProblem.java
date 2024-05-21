4d4cd1364655bc4d8fdf7bcfcac43ca0c7e31068
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-21 08:35:15 +0545

    Update N-QueensProblem.java

--- Backtracking/N-QueensProblem.java
+++ Backtracking/N-QueensProblem.java
@@ -82,6 +82,16 @@ public class NQueens {
 }
 
 /*
+Q . . . . . . .
+. . . . Q . . .
+. . . . . . . Q
+. . . . . Q . .
+. . Q . . . . .
+. . . . . . Q .
+. Q . . . . . .
+. . . Q . . . .
+*/
+/*
 Explanation
 Initialization:
 
