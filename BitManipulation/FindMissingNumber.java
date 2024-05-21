3560f1aa25fb89768bca3f0d80ab8abb4ece2c5c
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-20 23:16:44 -0700

    Create FindMissingNumber.java

---
+++ BitManipulation/FindMissingNumber.java
@@ -0,0 +1,17 @@
+// Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
+// find the one that is missing from the array.
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xor = n; // XOR with the index n since we have n+1 numbers including 0.
+        for (int i = 0; i < n; i++) {
+            xor ^= i ^ nums[i];
+        }
+        return xor;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 0, 1};
+        System.out.println("Missing number is: " + missingNumber(nums)); // Output: 2
+    }
+}
